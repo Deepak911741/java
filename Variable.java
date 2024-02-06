0497a5d7db81023afdf596c3811b1f94dbd2ee4d
Deepak911741 <dk367748@gmail.com>
2024-02-07 00:47:41 +0530

    java

---
+++ Variable.java
@@ -0,0 +1,44 @@
+import java.util.*;
+
+class Variable {
+    public static void main(String[] args) {
+        // String name = "Deepak kumar";
+        // int age = 50;
+        // int a = 20; // This is a variale
+        // int b = 30;
+        // double c = 25.36;
+
+
+        // int a = 10;
+        // int b = 5;
+
+        // int sum = a + b;
+        // int diff = a - b;
+        // int mul = a * b;
+        // int div = a / b;
+        // int mol = a % b;
+        // System.out.println(sum);
+        // System.out.println(diff);
+        // System.out.println(mul);
+        // System.out.println(div);
+        // System.out.println(mol);
+
+        // int ans = (a * b) / (a - b);
+        // System.out.println(ans);
+
+        Scanner sc = new Scanner(System.in);
+        String name = sc.nextLine();
+        System.out.println(name);
+        // nextInt
+        // nextFloat
+        System.out.println(name + " kumar");
+
+    }
+}
+
+
+// data types 
+
+//primitive => byte, short, char, boolean, int, long, float, double
+
+// Non-Primitive => String, Array, Class, Object, Interface 
