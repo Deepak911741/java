d7230fb07a5dab3ef67b4b883b33ce2f87872a73
Deepak911741 <dk367748@gmail.com>
2024-02-19 22:50:51 +0530

    operations

---
+++ Operations.java
@@ -0,0 +1,8 @@
+public class Operations {
+    public static void main(String[] args) {
+        double a = 3.4, b = 50.2, e = 44.5, c = 2.1, d = 0.55, f = 5.9;
+        double x = ((e*d)-(b*f))/((a*d)-(b*c));
+        double z = ((a*f)-(e*c))/((a*d)-(b*c));  
+        System.out.println(" x is" + x + "y is " + z);
+    }
+}
