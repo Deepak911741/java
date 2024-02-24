a11c6738fe2ef89f150ac8dfea8da7940ae00b02
Deepak911741 <dk367748@gmail.com>
2024-02-24 21:33:30 +0530

    javaMethods

---
+++ javamethod/Method1.java
@@ -0,0 +1,10 @@
+public class Method1 {
+    static void myMethod() {
+        System.out.println("I just got executed!");
+      }
+    
+      public static void main(String[] args) {
+        myMethod();
+      }
+    
+}
---
+++ javamethod/method_parmeter.java
@@ -0,0 +1,15 @@
+// package javamethod;
+
+public class method_parmeter {
+    public class Main {
+        static void myMethod(String fname, int age) {
+          System.out.println(fname + " is " + age);
+        }
+      
+        public static void main(String[] args) {
+          myMethod("Liam", 5);
+          myMethod("Jenny", 8);
+          myMethod("Anja", 31);
+        }
+      }
+}
